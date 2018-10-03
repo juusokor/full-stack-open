c939604bcf6fa9349fce88e8403f0a551a9b7bd2
Juuso Korhonen <juseek@gmail.com>
2018-10-03 21:14:02 +0300

    osa2 kurssit 2.1 - 2.3

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,106 +1,72 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Anecdote = ({ anecdotes, selected }) => {
+const Kurssi = ({ kurssi }) =>
+  <div>
+    <Otsikko kurssi={kurssi} />
+    <Sisalto kurssi={kurssi} />
+    <Yhteensa kurssi={kurssi} />
+  </div>
+
+const Otsikko = (props) =>
+  <div>
+    <h1>{props.kurssi.nimi}</h1>
+  </div>
+
+const Sisalto = ({ kurssi }) => {
+  const osaLista = kurssi.osat.map(osa => <Osa key={osa.id} osa={osa} />)
   return (
     <div>
-      {anecdotes[selected]}
+      {osaLista}
     </div>
   )
 }
 
-const AnecdoteVoteCount = ({ voteCount }) => {
+const Osa = ({ osa }) =>
+  <div>
+    <p>{osa.nimi} {osa.tehtavia}</p>
+  </div>
+
+const Yhteensa = ({ kurssi }) => {
+  const summa = kurssi.osat.reduce((sum, osa) => sum + osa.tehtavia, 0)
   return (
     <div>
-      has {voteCount} votes
+      <p>yhteensa {summa} tehtävää</p>
     </div>
   )
 }
 
-const AnecdoteWithMostVotes = ({ anecdotes, selected, pisteet }) => {
-  const topAnecdote = pisteet.reduce(function (a, b) { return a > b ? a : b })
-  return (
-    <div>
-      <Anecdote anecdotes={anecdotes} selected={pisteet[topAnecdote]} />
-      <AnecdoteVoteCount voteCount={topAnecdote} />
-    </div>
-  )
-}
-
-const Button = ({ handleClick, text }) => {
-  return (
-    <button onClick={handleClick}>{text}</button>
-  )
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      selected: 0,
-      pisteet: new Array(anecdotes.length).fill(0)
-    }
-  }
-
-  voteAnecdote = (prevState, index) => {
-    return () => {
-      const newPisteet = [...prevState]
-      newPisteet[index] += 1
-      this.setState(
-        {
-          pisteet: newPisteet
-        })
-    }
-  }
-
-  getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  chooseRandomAnecdote = () => {
-    this.setState({
-      selected: this.getRandomInt(0, anecdotes.length - 1)
-    })
-  }
-
-  render() {
-
-    const showStatsIfVotes = () => {
-      const sum = this.state.pisteet.reduce((a, b) => a + b, 0)
-      if (sum === 0) {
-        return (
-          <h2>vote to see the highest voted anecdote.</h2>
-        )
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10,
+        id: 1
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7,
+        id: 2
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14,
+        id: 3
       }
-      return (
-        <div>
-          <h2>anecdote with most votes:</h2>
-          <AnecdoteWithMostVotes anecdotes={this.props.anecdotes} selected={this.state.selected} pisteet={this.state.pisteet} />
-        </div>
-      )
-    }
-
-    return (
-      <div>
-        <Anecdote anecdotes={this.props.anecdotes} selected={this.state.selected} />
-        <AnecdoteVoteCount voteCount={this.state.pisteet[this.state.selected]} />
-        <Button handleClick={this.voteAnecdote(this.state.pisteet, this.state.selected)} text='vote' />
-        <Button handleClick={this.chooseRandomAnecdote} text='next anecdote' />
-        <div>{showStatsIfVotes()}</div>
-      </div >
-    )
+    ]
   }
+
+  return (
+    <div>
+      <Kurssi kurssi={kurssi} />
+    </div>
+  )
 }
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+
 
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App />,
   document.getElementById('root')
 )
