2fb812fdb33da29db968f913406832a58fae1d99
Juuso Korhonen <juseek@gmail.com>
2018-10-19 14:25:50 +0300

    osa2 puhelinluettelo 2.6-2.10

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,63 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Kurssi, Otsikko, Sisalto, Osa, Yhtensa } from './components/Kurssi'
-
-const KurssiLista = ({ kurssit }) => {
-  const kurssilista = kurssit.map(kurssi => <Kurssi key={kurssi.id} kurssi={kurssi} />)
-  return (
-    <div>
-      {kurssilista}
-    </div>
-  )
-}
-
-const App = () => {
-  const kurssit = [
-    {
-      nimi: 'Half Stack -sovelluskehitys',
-      id: 1,
-      osat: [
-        {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10,
-          id: 1
-        },
-        {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7,
-          id: 2
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14,
-          id: 3
-        }
-      ]
-    },
-    {
-      nimi: 'Node.js',
-      id: 2,
-      osat: [
-        {
-          nimi: 'Routing',
-          tehtavia: 3,
-          id: 1
-        },
-        {
-          nimi: 'Middlewaret',
-          tehtavia: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-
-  return (
-    <div>
-      <KurssiLista kurssit={kurssit} />
-    </div>
-  )
-}
+import App from './components/App'
 
 ReactDOM.render(
   <App />,
